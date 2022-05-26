813640bf718fef1ddaf384bd4fee98c4b8fead88
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-05-26 12:28:04 +0800

    ♻️ refactor(args): split args module

---
+++ src/bin/analyze-rs/args.rs
@@ -0,0 +1,111 @@
+use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(name = "analyze-rs")]
+#[clap(about = "analyze human GAIT cycle", long_about = None)]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// filter raw file
+    #[clap(arg_required_else_help = true)]
+    Filter(Filter),
+    /// export calculation in selection range
+    #[clap(arg_required_else_help = true)]
+    Export(Export),
+    /// write selection range into raw file
+    #[clap(arg_required_else_help = true)]
+    Swrite(Swrite),
+    /// concat export files
+    #[clap(arg_required_else_help = true)]
+    Concat(Concat),
+    /// auto select valid selection
+    #[clap(arg_required_else_help = true)]
+    Split(Split),
+    /// check if header and file num correct
+    #[clap(arg_required_else_help = true)]
+    Check(Check),
+}
+
+#[derive(Debug, Args)]
+pub struct Filter {
+    /// input file
+    #[clap(short, long, required = true)]
+    pub file: PathBuf,
+    /// output directory
+    #[clap(short, long, required = true)]
+    pub save: PathBuf,
+}
+
+#[derive(Debug, Args)]
+pub struct Export {
+    /// input file
+    #[clap(short, long, required = true)]
+    pub file: PathBuf,
+    /// output directory
+    #[clap(short, long, required = true)]
+    pub save: PathBuf,
+    /// selection range index. e.g. "2 5"
+    #[clap(short, long, parse(try_from_str = parse_range_tuple), required = true)]
+    pub ranges: Vec<(u32, u32)>,
+}
+
+#[derive(Debug, Args)]
+pub struct Swrite {
+    /// input file
+    #[clap(short, long, required = true)]
+    pub file: PathBuf,
+    /// output directory
+    #[clap(short, long, required = true)]
+    pub save: PathBuf,
+    /// selection values to write
+    #[clap(short, long, required = true)]
+    pub value: String,
+}
+
+#[derive(Debug, Args)]
+pub struct Concat {
+    /// inputs file (can be multiple, e.g. "-f file1 -f file2")
+    #[clap(short, long, parse(from_os_str), required = true)]
+    pub file: Vec<PathBuf>,
+    /// output directory
+    #[clap(short, long, required = true)]
+    pub save: PathBuf,
+}
+
+#[derive(Debug, Args)]
+pub struct Split {
+    /// input directory
+    #[clap(short, long, required = true)]
+    pub file_dir: PathBuf,
+    /// output directory
+    #[clap(short, long, required = true)]
+    pub save: PathBuf,
+    /// valid percentage
+    #[clap(short, long, required = true)]
+    pub percent: usize,
+}
+
+#[derive(Debug, Args)]
+pub struct Check {
+    /// input directory
+    #[clap(short, long, required = true)]
+    pub file_dir: PathBuf,
+}
+
+fn parse_range_tuple<T, U>(
+    s: &str,
+) -> Result<(T, U), Box<dyn std::error::Error + Send + Sync + 'static>>
+where
+    T: std::str::FromStr,
+    T::Err: std::error::Error + Send + Sync + 'static,
+    U: std::str::FromStr,
+    U::Err: std::error::Error + Send + Sync + 'static,
+{
+    let vec = s.split(' ').take(2).collect::<Vec<&str>>();
+    Ok((vec[0].parse()?, vec[1].parse()?))
+}
--- src/bin/analyze-rs/main.rs
+++ src/bin/analyze-rs/main.rs
@@ -1,15 +1,16 @@
 mod core;
 mod utils;
+mod args;
 
-use self::core::concat::concater;
-use self::core::export::exporter;
-use self::core::filter::filter;
-use self::core::swrite::swrite;
-use self::core::split::split;
-use self::core::check::check;
+use crate::args::*;
+use crate::core::concat::concater;
+use crate::core::export::exporter;
+use crate::core::filter::filter;
+use crate::core::swrite::swrite;
+use crate::core::split::split;
+use crate::core::check::check;
 
-use clap::{Args, Parser, Subcommand};
-use std::path::PathBuf;
+use clap::Parser;
 
 ///
 /// Example command and output
@@ -35,114 +36,6 @@ use std::path::PathBuf;
 /// Response: {"ConcatFile":"concat.csv"}
 ///
 
-#[derive(Debug, Parser)]
-#[clap(name = "analyze-rs")]
-#[clap(about = "analyze human GAIT cycle", long_about = None)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    /// filter raw file
-    #[clap(arg_required_else_help = true)]
-    Filter(Filter),
-    /// export calculation in selection range
-    #[clap(arg_required_else_help = true)]
-    Export(Export),
-    /// write selection range into raw file
-    #[clap(arg_required_else_help = true)]
-    Swrite(Swrite),
-    /// concat export files
-    #[clap(arg_required_else_help = true)]
-    Concat(Concat),
-    /// auto select valid selection
-    #[clap(arg_required_else_help = true)]
-    Split(Split),
-    /// check if header and file num correct
-    #[clap(arg_required_else_help = true)]
-    Check(Check),
-}
-
-#[derive(Debug, Args)]
-struct Filter {
-    /// input file
-    #[clap(short, long, required = true)]
-    file: PathBuf,
-    /// output directory
-    #[clap(short, long, required = true)]
-    save: PathBuf,
-}
-
-#[derive(Debug, Args)]
-struct Export {
-    /// input file
-    #[clap(short, long, required = true)]
-    file: PathBuf,
-    /// output directory
-    #[clap(short, long, required = true)]
-    save: PathBuf,
-    /// selection range index. e.g. "2 5"
-    #[clap(short, long, parse(try_from_str = parse_range_tuple), required = true)]
-    ranges: Vec<(u32, u32)>,
-}
-
-#[derive(Debug, Args)]
-struct Swrite {
-    /// input file
-    #[clap(short, long, required = true)]
-    file: PathBuf,
-    /// output directory
-    #[clap(short, long, required = true)]
-    save: PathBuf,
-    /// selection values to write
-    #[clap(short, long, required = true)]
-    value: String,
-}
-
-#[derive(Debug, Args)]
-struct Concat {
-    /// inputs file (can be multiple, e.g. "-f file1 -f file2")
-    #[clap(short, long, parse(from_os_str), required = true)]
-    file: Vec<PathBuf>,
-    /// output directory
-    #[clap(short, long, required = true)]
-    save: PathBuf,
-}
-
-#[derive(Debug, Args)]
-struct Split {
-    /// input directory
-    #[clap(short, long, required = true)]
-    file_dir: PathBuf,
-    /// output directory
-    #[clap(short, long, required = true)]
-    save: PathBuf,
-    /// valid percentage
-    #[clap(short, long, required = true)]
-    percent: usize,
-}
-
-#[derive(Debug, Args)]
-struct Check {
-    /// input directory
-    #[clap(short, long, required = true)]
-    file_dir: PathBuf,
-}
-
-fn parse_range_tuple<T, U>(
-    s: &str,
-) -> Result<(T, U), Box<dyn std::error::Error + Send + Sync + 'static>>
-where
-    T: std::str::FromStr,
-    T::Err: std::error::Error + Send + Sync + 'static,
-    U: std::str::FromStr,
-    U::Err: std::error::Error + Send + Sync + 'static,
-{
-    let vec = s.split(' ').take(2).collect::<Vec<&str>>();
-    Ok((vec[0].parse()?, vec[1].parse()?))
-}
 
 fn main() {
     let args = Cli::parse();
